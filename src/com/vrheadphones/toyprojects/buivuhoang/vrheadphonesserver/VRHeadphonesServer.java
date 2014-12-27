129f3de85ee50896fd86079e9a52c93a61f2b97b
Bui Vu Hoang <buivuhoang15@gmail.com>
2014-12-27 00:18:14 +0000

    Working server - Receives rotation angles - Displays the cube's position on the UI

--- src/com/vrheadphones/toyprojects/buivuhoang/vrheadphonesserver/VRHeadphonesServer.java
+++ src/com/vrheadphones/toyprojects/buivuhoang/vrheadphonesserver/VRHeadphonesServer.java
@@ -1,12 +1,18 @@
 package com.vrheadphones.toyprojects.buivuhoang.vrheadphonesserver;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.InetAddress;
 import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.jar.JarFile;
 
+import javax.swing.Timer;
+
 public class VRHeadphonesServer {
+	private float[] originalCubeCentre = {0, 4, 0};
+	private float[] currentCubeCentre = {0, 4, 0};
 	
 	public static JarFile jar;
 	public static String basePath = "";
@@ -14,6 +20,9 @@ public class VRHeadphonesServer {
 	private String[] textLines = new String[6];
 	private String sHost = "";
 	
+	private Timer timer;
+	private static AppMainView appMainView;
+	private OSCWorld world;
 	public VRHeadphonesServer() {
 
 		// get local IP
@@ -43,26 +52,52 @@ public class VRHeadphonesServer {
 
 		}
 		
+		world = new OSCWorld(this);
+		
 		// Schedule a job for the event dispatch thread:
 		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				AppMainView appMainView = new AppMainView();
-				appMainView.setIpAddress(sHost);
+				appMainView = new AppMainView();
 				appMainView.createAndShowGUI();
-
+				appMainView.setIpAddress(sHost);
 				appMainView.updateRotationalData(1, 2, 3);
 			}
 		});
 		
 	}
 	
+	public void updateRotationAngle(float thetaX, float thetaY, float thetaZ) {
+		float x = originalCubeCentre[0];
+        float y = originalCubeCentre[1];
+        currentCubeCentre[0] = (float) (x * Math.cos(thetaX) - y * Math.sin(thetaX));
+        currentCubeCentre[1] = (float) (x * Math.sin(thetaX) + y * Math.cos(thetaX));
+
+        y = currentCubeCentre[1];
+        float z = originalCubeCentre[2];
+        currentCubeCentre[1] = (float) (y * Math.cos(thetaZ) - z * Math.sin(thetaZ));
+        currentCubeCentre[2] = (float) (y * Math.sin(thetaZ) + z * Math.cos(thetaZ));
+
+        x = currentCubeCentre[0];
+        z = currentCubeCentre[2];
+        currentCubeCentre[0] = (float) (  x * Math.cos(thetaY) - z * Math.sin(thetaY));
+        currentCubeCentre[2] = (float) (- x * Math.sin(thetaY) + z * Math.cos(thetaY));
+        
+        appMainView.updateRotationalData(x, y, z);
+//        appMainView.updateRotationalData(thetaX, thetaY, thetaZ);
+	}
+	
 	public void start() {
-		// TODO: implement the server routine
+		this.timer = new Timer(500, new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				world.onEnter();
+				timer.stop();
+			}
+		});
+		this.timer.start();
 	}
 	
 	public static void main(String[] args) {
-		
 		VRHeadphonesServer server = new VRHeadphonesServer();
 		server.start();
 	}
